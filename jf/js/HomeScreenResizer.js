1967d260d25392fe9fe57ba8a3877c00128decee
Juan Meza <me@jrmeza.com>
2014-03-27 21:17:09 -0700

    asd
    asd

---
+++ jf/js/HomeScreenResizer.js
@@ -0,0 +1,18 @@
+ // JavaScript Document
+ $( document ).ready(function(e) {
+     (function(){
+		var resize = function(){
+			var headerElement = document.getElementById("home-section");
+			var w = window.innerWidth;
+			var h = w * 0.6;
+			if( h > window.innerHeight){
+				h = window.innerHeight;
+			}
+			headerElement.style.height = h + "px";
+			//headerElement.style.width = w + "px";
+		}
+		window.addEventListener("resize", resize, false);
+		resize();
+ })();
+});
+ 
