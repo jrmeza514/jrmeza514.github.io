203274eff9828aed3b9d96774edcaba4d0591a0d
Juan Meza <jrmeza514@gmail.com>
2015-10-29 10:01:40 -0700

    SO Far so good!

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@
 */
 $(document).ready(function(){
 	ko.applyBindings();
+	$("#toggle").click( function(){
+		$("nav").toggleClass("toggled");
+	});
+	$("header a").click(function(){
+		$("nav").removeClass("toggled");
+	});
+	$(window).resize(function(){
+		$("nav").removeClass("toggled");
+	});
 });
