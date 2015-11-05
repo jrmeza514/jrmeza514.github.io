e2a79675b9f6406ae61ad49fd7ea2e13850e28ef
Juan Meza <jrmeza514@gmail.com>
2015-11-05 15:51:22 -0800

    Commit Changes
    Added Unit Circle Project

--- js/portfolio.js
+++ js/portfolio.js
@@ -20,6 +20,12 @@
 			description: "The application consists of three main components: a Google Map, a Third Party API, and  a responsive design. The goal of this project was to add some of my favorite locations as markers on a Google Map. When the user clicks on the marker,  the most recent instagram posts using the hashtag for the corresponding location will load into the user interface. The images show as a horizontally-scrollable list of images at the bottom of the screen. Finally, all this content will  adapt and remain usable at various screen sizes. "
 		},
 		{
+			name: "Interactive Unit Circle",
+			url: "http://jrmeza514.github.io/trig-unit-circle/",
+			img: "img/unit_circle.png",
+			description: "This is an Interactive unit circle"
+		},
+		{
 			name: "Unit Testing",
 			url: "http://jrmeza514.github.io/frontend-nanodegree-feedreader/",
 			img: "img/tests.png",
