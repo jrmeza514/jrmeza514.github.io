017878f7b5778a3ceef42d47f9d04400bbc6d343
Juan Meza <jrmeza514@gmail.com>
2015-11-05 16:20:18 -0800

    Added Github Links
    The Portfolio Projects now show links for both the live web apps and
    github repositories.

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,30 +4,35 @@
 		{
 			name: "Neighborhood Map",
 			url: "http://jrmeza514.github.io/frontend-nanodegree-neighborhood-app/",
+			github: "https://github.com/jrmeza514/frontend-nanodegree-neighborhood-app/",
 			img: "img/map.png",
 			description: "The application consists of three main components: a Google Map, a Third Party API, and  a responsive design. The goal of this project was to add some of my favorite locations as markers on a Google Map. When the user clicks on the marker,  the most recent instagram posts using the hashtag for the corresponding location will load into the user interface. The images show as a horizontally-scrollable list of images at the bottom of the screen. Finally, all this content will  adapt and remain usable at various screen sizes. "
 		},
 		{
 			name: "Arcade Game Clone",
 			url: "http://jrmeza514.github.io/frontend-nanodegree-arcade-game/",
+			github: "https://github.com/jrmeza514/frontend-nanodegree-arcade-game/",
 			img: "img/game.png",
 			description: "The application consists of three main components: a Google Map, a Third Party API, and  a responsive design. The goal of this project was to add some of my favorite locations as markers on a Google Map. When the user clicks on the marker,  the most recent instagram posts using the hashtag for the corresponding location will load into the user interface. The images show as a horizontally-scrollable list of images at the bottom of the screen. Finally, all this content will  adapt and remain usable at various screen sizes. "
 		},
 		{
 			name: "Web Optimizations",
 			url: "http://jrmeza514.github.io/frontend-nanodegree-mobile-portfolio/",
+			github: "https://github.com/jrmeza514/frontend-nanodegree-mobile-portfolio/",
 			img: "img/optim.png",
 			description: "The application consists of three main components: a Google Map, a Third Party API, and  a responsive design. The goal of this project was to add some of my favorite locations as markers on a Google Map. When the user clicks on the marker,  the most recent instagram posts using the hashtag for the corresponding location will load into the user interface. The images show as a horizontally-scrollable list of images at the bottom of the screen. Finally, all this content will  adapt and remain usable at various screen sizes. "
 		},
 		{
 			name: "Interactive Unit Circle",
 			url: "http://jrmeza514.github.io/trig-unit-circle/",
+			github: "https://github.com/jrmeza514/trig-unit-circle/",
 			img: "img/unit_circle.png",
 			description: "This is an Interactive unit circle"
 		},
 		{
 			name: "Unit Testing",
 			url: "http://jrmeza514.github.io/frontend-nanodegree-feedreader/",
+			github: "https://github.com/jrmeza514/frontend-nanodegree-feedreader/",
 			img: "img/tests.png",
 			description: "The application consists of three main components: a Google Map, a Third Party API, and  a responsive design. The goal of this project was to add some of my favorite locations as markers on a Google Map. When the user clicks on the marker,  the most recent instagram posts using the hashtag for the corresponding location will load into the user interface. The images show as a horizontally-scrollable list of images at the bottom of the screen. Finally, all this content will  adapt and remain usable at various screen sizes. "
 		}
